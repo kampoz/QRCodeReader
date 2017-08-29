ba879ef2b8fb3f30237972224a425a5fb8da59aa
Kamil Poznakowski <Kamil@Mac-mini-Kamil.local>
2017-08-29 23:29:58 +0200

    Skanuje tylko raz, jeden dialog z jedna wiadomoscia

--- app/src/main/java/com/example/kamil/qrcodereader/MainActivity.java
+++ app/src/main/java/com/example/kamil/qrcodereader/MainActivity.java
@@ -1,32 +1,51 @@
 package com.example.kamil.qrcodereader;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import com.google.zxing.Result;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
   private ZXingScannerView mScannerView;
+  private Button btnScanQR;
+  private String result;
+  private AlertDialog alert1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    btnScanQR = (Button) findViewById(R.id.btn_Scan_QR);
+    btnScanQR.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        QrScanner();
+      }
+    });
+
   }
 
-  public void QrScanner(View view){
+  public void QrScanner(View view) {
 
     mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
     setContentView(mScannerView);
+    mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+    mScannerView.startCamera();         // Start camera
 
+  }
+
+  public void QrScanner() {
+
+    mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+    setContentView(mScannerView);
     mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
     mScannerView.startCamera();         // Start camera
 
@@ -35,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
   @Override
   public void onPause() {
     super.onPause();
-    mScannerView.stopCamera();           // Stop camera on pause
+    //mScannerView.stopCamera();           // Stop camera on pause
   }
 
   @Override
@@ -48,11 +67,28 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     // show the scanner result into dialog box.
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
     builder.setTitle("Scan Result");
+    String newResult = rawResult.getText();
+    //if ((result == null) || result != null && !newResult.equals(result)) {
+
+    result = newResult;
     builder.setMessage(rawResult.getText());
-    AlertDialog alert1 = builder.create();
-    alert1.show();
+    builder.setPositiveButton("OK",
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int witch) {
+            dialog.dismiss();
+            alert1 = null;
+            //recreate();
+          }
+        });
+    if (alert1 == null) {
+      alert1 = builder.create();
+      alert1.show();
+    }
+    //}
 
     // If you would like to resume scanning, call this method below:
-    // mScannerView.resumeCameraPreview(this);
+    mScannerView.resumeCameraPreview(this);
+
   }
 }
